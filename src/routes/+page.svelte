c2b26344aacfde7d3d52aaa578a061e9a5043e73
Dmytro Cheglakov <dcheglakov@gmail.com>
2023-06-17 22:15:59 +0300

    home page styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
-<h1>Welcome to Melt UI Demos</h1>
-<p>Visit <a href="https://www.melt-ui.com/">Melt UI</a> to read the documentation</p>
+<main class="p-4">
+	<header class="py-4">
+		<h1 class="text-3xl font-medium">Welcome to Melt UI Demos</h1>
+		<p>Visit <a href="https://www.melt-ui.com/">Melt UI</a> to read the documentation</p>
+	</header>
 
-<section>
-	<h2>Demos</h2>
-	<a href="/demos/accordion">
-		<article>Accordion</article>
-	</a>
-</section>
+	<section class="flex flex-col gap-3">
+		<h2 class="text-2xl font-medium">Demos</h2>
+		<div class="grid grid-cols-3 gap-3">
+			<a href="/demos/accordion" class="bg-gray-50 p-4 rounded-lg">
+				<article>Accordion</article>
+			</a>
+			<a href="/demos/dialog" class="bg-gray-50 p-4 rounded-lg">
+				<article>Dialog</article>
+			</a>
+			<a href="/demos/select" class="bg-gray-50 p-4 rounded-lg">
+				<article>Select</article>
+			</a>
+		</div>
+	</section>
+</main>
